7b55c8f2fbb577c3a1f13db96e309e10a5d87a24
Alexey Vorobyov <zenti.asteris@gmail.com>
2016-05-16 18:41:22 +0300

    Restructuring: move practice files to distinct package

---
+++ itmo/src/com/practice/Pract31Servlet.java
@@ -0,0 +1,17 @@
+package com.practice;
+
+import java.io.IOException;
+
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Pract31Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		double m = Double.parseDouble(req.getParameter("m"));
+		double n = Double.parseDouble(req.getParameter("n"));
+		double result = Math.abs(m - 10) < Math.abs(n - 10) ? m : n;
+		resp.getWriter().println(Double.toString(result));
+	}
+}
